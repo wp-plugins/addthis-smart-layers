8c55e19d75cb520e070e355b0cbbe9404b95425e
srijith.v <srijith.v@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-16 14:54:23 +0000

    Fix for missed escape chars in some servers.
    git-svn-id: https://plugins.svn.wordpress.org/addthis-smart-layers/trunk@788820 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/smart_layer_include.php
+++ views/smart_layer_include.php
@@ -2,7 +2,7 @@
 
 global $addthis_addjs;
 
-$addthis_smart_layer_include = 'addthis.layers(' . get_option('smart_layer_settings') . ');';
+$addthis_smart_layer_include = 'addthis.layers(' . stripslashes(get_option('smart_layer_settings')) . ');';
 $addthis_addjs->addAfterScript($addthis_smart_layer_include);
 $addthis_addjs->output_script();
 
